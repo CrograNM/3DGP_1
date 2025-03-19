90eb3ba1916c31cd2478245af4c457df5e03e8cd
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-03-19 21:43:40 +0900

    따라하기02

--- LabProjects/LabProject01/LabProject01.cpp
+++ LabProjects/LabProject01/LabProject01.cpp
@@ -95,20 +95,24 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+    DWORD dwStyle = WS_OVERLAPPED | WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU | WS_BORDER;
+    RECT rc = { 0, 0, 640, 480 };
+    AdjustWindowRect(&rc, dwStyle, FALSE);
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
+    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+    
+    HWND hWnd = CreateWindow(szWindowClass, szTitle, dwStyle, CW_USEDEFAULT, CW_USEDEFAULT, 
+                            rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
+    
+    if (!hWnd)
+    {
+       return FALSE;
+    }
+    
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    
+    return TRUE;
 }
 
 //
@@ -123,6 +127,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+
     switch (message)
     {
     case WM_COMMAND:
